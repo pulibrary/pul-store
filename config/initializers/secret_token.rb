6e9af90baca9ceee81bc9858fa4d121fc149e115
Jon Stroop <jpstroop@gmail.com>
2013-10-17 12:25:44 -0400

    blank, empty, new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PulStore::Application.config.secret_key_base = '4578038ae6b98f05507150ac5d775350616f119111b0de5357fff602b16c167f8b22d37b1dae6c30c741c0e8dd235571182cefe5d76c001593234b2ea6994846'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PulStore::Application.config.session_store :cookie_store, key: '_pul-store_session'
