d03e355b89d4242636628f341034377412cd2609
Jon Stroop <jpstroop@gmail.com>
2013-10-18 16:38:07 -0400

    dates on save

--- spec/factories/text.rb
+++ spec/factories/text.rb
@@ -1,13 +1,14 @@
 require 'faker'
 
-
 FactoryGirl.define do
   factory :text do |t|
     t.type "Text"
     t.title { Faker::Lorem.sentence(3, true, 5) }
     t.sort_title { Faker::Lorem.sentence(2, true, 5) }
-    t.description { Faker::Lorem.sentence(3, true, 5) }
-    t.subject { Faker::Lorem.sentence(3, true, 5) }
-    t.language { Faker::Lorem.sentence(3, true, 5) }
+    t.date_uploaded DateTime.now.utc # probably should be randomized
+    t.date_modified DateTime.now.utc # probably should be randomized
+    t.description { Faker::Lorem.sentence(5, true, 3) }
+    t.subject { Faker::Lorem.sentence(5, true, 3) }
+    t.language { Faker::Lorem.words(1) }
   end
 end
