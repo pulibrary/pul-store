7facd65af282b4f85d4ee4ab10ff08d14cc6a9af
Jon Stroop <jpstroop@gmail.com>
2013-10-17 20:11:26 -0400

    added validations

--- text.rb
+++ text.rb
@@ -1,7 +1,12 @@
 class Text < Work
   has_metadata 'text_rdf_metadata', type: TextRdfMetadata
 
-  has_many :pages, :property=> :is_part_of
+  has_many :pages, property: :is_part_of
+
+  validates :type, inclusion: { 
+    in: ['Text'],
+    message: "%{value} is not a valid value for type"
+  }
 
   delegate :description, to: 'text_rdf_metadata', multiple: true
   delegate :subject, to: 'text_rdf_metadata', multiple: true
--- work.rb
+++ work.rb
@@ -1,11 +1,20 @@
 class Work < ActiveFedora::Base
   has_metadata 'work_rdf_metadata', type: WorkRdfMetadata
 
+  # TODO: needs to get a PID, date_modified and possibly date_uploaded just before save
+  # see http://stackoverflow.com/a/6770431/714478
+
+  validates :type, presence: true
+  validates :title, presence: true
+  validates :pid, presence: true
+  validates_with CreatorContributorValidator
+
+  delegate :type, to: 'work_rdf_metadata', multiple: false
   delegate :title, to: 'work_rdf_metadata', multiple: false
   delegate :creator, to: 'work_rdf_metadata', multiple: false
-  delegate :type, to: 'work_rdf_metadata', multiple: false
   delegate :contributor, to: 'work_rdf_metadata', multiple: true
   delegate :date_uploaded, to: 'work_rdf_metadata', multiple: false
   delegate :date_modified, to: 'work_rdf_metadata', multiple: false
 end
+
 
