9dc21b36aa3dc765e243c6d773a0b34f91c41de6
Jon Stroop <jpstroop@gmail.com>
2013-10-18 11:05:59 -0400

    moved tests back out

--- work.rb
+++ work.rb
@@ -14,6 +14,7 @@ class Work < ActiveFedora::Base
   delegate :sort_title, to: 'work_rdf_metadata', multiple: false
   delegate :creator, to: 'work_rdf_metadata', multiple: false
   delegate :contributor, to: 'work_rdf_metadata', multiple: true
+  
   delegate :date_uploaded, to: 'work_rdf_metadata', multiple: false
   delegate :date_modified, to: 'work_rdf_metadata', multiple: false
 end
