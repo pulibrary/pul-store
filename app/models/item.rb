81fb57c8d1c4bd21ea44db8d2b6f9c4339583828
Jon Stroop <jpstroop@gmail.com>
2013-10-27 14:14:41 -0400

    descMetadata stream should alsways have the same name

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,24 @@
 class Item  < ActiveFedora::Base
-  has_metadata 'item_rdf_metadata', type: ItemRdfMetadata
+  has_metadata 'descMetadata', type: ItemRdfMetadata
 
-  # validates :title, presence: true
-  # validates :sort_title, presence: true
-  # validates :type, presence: true
+  validates :title, presence: true
+  validates :sort_title, presence: true
+  validates :type, presence: true
 
   # validates_with CreatorContributorValidator
   
   # validates :date_modified, presence: true
   # validates :date_uploaded, presence: true
 
-  delegate :type, to: 'item_rdf_metadata', multiple: false
-  delegate :title, to: 'item_rdf_metadata', multiple: false
-  delegate :sort_title, to: 'item_rdf_metadata', multiple: false
-  delegate :creator, to: 'item_rdf_metadata', multiple: false
-  delegate :contributor, to: 'item_rdf_metadata', multiple: true
-  delegate :date_created, to: 'item_rdf_metadata', multiple: false
+  delegate :type, to: 'descMetadata', multiple: false
+  delegate :title, to: 'descMetadata', multiple: false
+  delegate :sort_title, to: 'descMetadata', multiple: false
+  delegate :creator, to: 'descMetadata', multiple: false
+  delegate :contributor, to: 'descMetadata', multiple: true
+  delegate :date_created, to: 'descMetadata', multiple: false
 
-  delegate :date_uploaded, to: 'item_rdf_metadata', multiple: false
-  delegate :date_modified, to: 'item_rdf_metadata', multiple: false
+  # TODO: these should move into a separate stream (needed by, e.g. page, 
+  # and not really descriptive)
+  delegate :date_uploaded, to: 'descMetadata', multiple: false
+  delegate :date_modified, to: 'descMetadata', multiple: false
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -2,18 +2,13 @@ class Text < Item
 
   has_many :pages, property: :is_part_of
 
-  has_metadata 'text_rdf_metadata', type: TextRdfMetadata
+  # has_metadata 'text_rdf_metadata', type: TextRdfMetadata
+  has_metadata 'descMetadata', type: TextRdfMetadata
 
   # validates :language, presence: true
   
-  # delegate :type, to: 'text_rdf_metadata', multiple: false
-  # delegate :title, to: 'text_rdf_metadata', multiple: false
-  # delegate :sort_title, to: 'text_rdf_metadata', multiple: false
-  # delegate :creator, to: 'text_rdf_metadata', multiple: false
-  # delegate :contributor, to: 'text_rdf_metadata', multiple: true
-
-  delegate :toc, to: 'text_rdf_metadata', multiple: false
-  delegate :description, to: 'text_rdf_metadata', multiple: true
-  delegate :subject, to: 'text_rdf_metadata', multiple: true
-  delegate :language, to: 'text_rdf_metadata', multiple: true
+  delegate :toc, to: 'descMetadata', multiple: false
+  delegate :description, to: 'descMetadata', multiple: true
+  delegate :subject, to: 'descMetadata', multiple: true
+  delegate :language, to: 'descMetadata', multiple: true
 end
