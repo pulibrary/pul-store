58de1a20d4a329b44d9dae220354e6930bf0c9b0
Jon Stroop <jpstroop@gmail.com>
2013-10-28 09:20:39 -0400

    active fedora models back in submodule

--- lib/rdf/item_properties.rb
+++
@@ -1,5 +0,0 @@
-module RDF
-  class PULItemWorks < Vocabulary("http://princeton.edu/pulstore/works/")
-    property :sortTitle
-  end
-end
--- lib/rdf/page_properties.rb
+++
@@ -1,5 +0,0 @@
-module RDF
-  class PULStorePages < Vocabulary("http://princeton.edu/pulstore/pages/")
-    property :sortOrder
-  end
-end
