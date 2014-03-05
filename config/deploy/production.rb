093837574f5780fa859618e116c01ce95c9790ff
Kevin Reiss <kevin.reiss@gmail.com>
2014-03-05 14:12:39 -0500

    pulstore1 becomes pustore2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :production
 set :rails_env, 'production'
-server 'pulstore1', roles: [:web, :app, :db]
+server 'pulstore2', roles: [:web, :app, :db]
 
 # Simple Role Syntax
 # ==================
