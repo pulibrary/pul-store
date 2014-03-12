69f973d1d4f8fbf8150a5e98ed9a528cff41fe23
Jon Stroop <jpstroop@gmail.com>
2014-03-12 19:36:17 -0400

    travis not seeing config as global?

--- config/initializers/pul_store_config.rb
+++ config/initializers/pul_store_config.rb
@@ -1,3 +1,4 @@
+cfg = YAML.load_file(Rails.root.join('config', 'pul_store.yml'))[Rails.env]
 Hydra::FileCharacterization.configure do |config|
-  config.tool_path(:fits, Rails.root.join(PUL_STORE_CONFIG['fits_dir'], 'fits.sh'))
+  config.tool_path(:fits, Rails.root.join(cfg['fits_dir'], 'fits.sh'))
 end
