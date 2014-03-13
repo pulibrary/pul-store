c0ae50371800ecd3ef80dd81ae31db83d5c69453
Jon Stroop <jpstroop@gmail.com>
2014-03-12 20:20:44 -0400

    use global config

--- config/initializers/pul_store_config.rb
+++ config/initializers/pul_store_config.rb
@@ -1,4 +1,3 @@
-cfg = YAML.load_file(Rails.root.join('config', 'pul_store.yml'))[Rails.env]
 Hydra::FileCharacterization.configure do |config|
   config.tool_path(:fits, Rails.root.join(cfg['fits_dir'], 'fits.sh'))
 end
