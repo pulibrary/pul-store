fece35aaf86b77b78a5434cd4c0622935203e6de
Kevin Reiss <kevin.reiss@gmail.com>
2014-03-20 00:47:57 -0400

    make language repeatable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311201626) do
+ActiveRecord::Schema.define(version: 20140319175813) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",     null: false
@@ -34,12 +34,25 @@ ActiveRecord::Schema.define(version: 20140311201626) do
     t.string "media_type"
   end
 
-  create_table "pul_store_lae_genres", force: true do |t|
-    t.string "pul_label"
-    t.string "tgm_label"
-    t.string "lcsh_label"
+  create_table "pul_store_lae_areas", force: true do |t|
+    t.string "label"
+    t.string "iso_3166_2_code"
+    t.string "gac_code"
     t.string "uri"
-    t.text   "scope_note"
+  end
+
+  add_index "pul_store_lae_areas", ["label"], name: "index_pul_store_lae_areas_on_label", unique: true, using: :btree
+
+  create_table "pul_store_lae_genres", force: true do |t|
+    t.string   "pul_label"
+    t.string   "tgm_label"
+    t.string   "lcsh_label"
+    t.integer  "dimensions"
+    t.string   "dimensions_unit"
+    t.string   "uri"
+    t.text     "scope_note"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "pul_store_lae_subjects", force: true do |t|
