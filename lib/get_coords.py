64f3b74d07a0a09884ca53a48405c6de303a01e6
Jon Stroop <jpstroop@gmail.com>
2014-04-04 11:55:40 -0400

    closes #173, added countries

--- lib/get_coords.py
+++ lib/get_coords.py
@@ -7,13 +7,14 @@ import json
 GEONAMES_UNAME = ''
 SEARCH_URI = 'http://api.geonames.org/searchJSON' #?q=london&maxRows=10&username=demo
 INFO_URI = 'http://api.geonames.org/countryInfoJSON' #?formatted=true&lang=it&country=DE&username=demo&style=full
-AREAS_IN = '/home/jstroop/workspace/pul-store/db/fixtures/lae_areas.csv'
-OUTPUT = '/home/jstroop/Desktop/lae_areas_enhanced.csv'
+AREAS_IN = '/home/jstroop/Desktop/new_areas.csv'
+OUTPUT = '/home/jstroop/Desktop/lae_areas.csv'
 
 def info_for_code(code):
     params = { 'country': code, 'username': GEONAMES_UNAME }
     r = get(INFO_URI, params=params, allow_redirects=False)
     return json.loads(r.text)['geonames'][0]#headers[URI_HEADER] if r.status_code == 302 else None
+
 
 with open(AREAS_IN, 'rb') as area_file, open(OUTPUT, 'wb') as output_file:
     reader = csv.reader(area_file)
