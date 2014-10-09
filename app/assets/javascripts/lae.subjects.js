6a4ae4b74179e07135ec857ee05d09d876725132
Kevin Reiss <kevin.reiss@gmail.com>
2014-10-09 12:41:17 -0400

    Update javascript string handling to deal with single quoted string handling. closes #278,#308

--- app/assets/javascripts/lae.subjects.js
+++ app/assets/javascripts/lae.subjects.js
@@ -80,7 +80,7 @@
           var subject_labels = _.pluck(subjects, 'label');
           var opt_list = "<option value>Select Subject</a>";
           _.each(subject_labels, function(label) {
-            opt_list = opt_list + "<option value='"+label+"'>"+label+"</option>";
+            opt_list = opt_list + "<option value=\"" + label +"\">"+label+"</option>";
           });
           //var add_category_button = '<button class="adder btn" id="additional_subject_submit" name="additional_subject">+<span class="sr-only">add another subject</span></button>';
           var subject_select = "<select class='form-control lae-subject' id='lae_folder_subject_' name='lae_folder[subject][]'>"+opt_list+"</select>";
