pulibrary/pul-store

commit 210cebf49a1a9fcfc263f4e1d06c5f4676206bdb

Add some model clauses to RSOLR queries to keep responses to the model desired. Closes #341.

app/controllers/concerns/pul_store/lae/folder_lookups.rb
@@ -10,7 +10,7 @@ module PulStore::Lae::FolderLookups
     def get_folder_list_by_box(box_id, docs = 20)
       solr = RSolr.connect(Blacklight.solr_config)
       fields_to_return = 'prov_metadata__physical_number_isi,prov_metadata__workflow_state_tesim,id,desc_metadata__title_tesim,desc_metadata__genre_tesim,prov_metadata__barcode_tesi'
-      solr_response = solr.get 'select', :params => {:q => "in_box_ssim:info:fedora/#{box_id}", 
+      solr_response = solr.get 'select', :params => {:q => "in_box_ssim:info:fedora/#{box_id} AND active_fedora_model_ssi:PulStore::Lae::Folder", 
                                                      :fl => fields_to_return, 
                                                      :start => 0, 
                                                      :rows => docs, 

app/controllers/concerns/pul_store/lae/page_lookups.rb
@@ -6,7 +6,7 @@ module PulStore::Lae::PageLookups
       solr = RSolr.connect(Blacklight.solr_config)
       # FIXME Get a default Where is the page sequence stored in index for this object?
       # sort by physical_number/sort_order
-      solr_response = solr.get 'select', :params => {:q => "is_part_of_ssim:info:fedora/#{folder_id}", 
+      solr_response = solr.get 'select', :params => {:q => "is_part_of_ssim:info:fedora/#{folder_id} AND active_fedora_model_ssi:PulStore::Page", 
                                                       :start => 0, 
                                                       :rows => 400, 
                                                       :wt => :ruby, 

commit 218873ada34743dcc98f3c45637c37efd6702a80

Hotfix for regression introduced in 210cebf49a1a9fcfc263f4e1d06c5f4676206bdb

app/controllers/concerns/pul_store/lae/folder_lookups.rb
@@ -7,17 +7,21 @@ module PulStore::Lae::FolderLookups
       PulStore::Lae::Folder.where(in_box_ssim: "#{fedora_prefix}#{box_id}").order('prov_metadata__physical_number_ssi asc')
     end
 
+    
     def get_folder_list_by_box(box_id, docs = 20)
-      solr = RSolr.connect(Blacklight.solr_config)
-      fields_to_return = 'prov_metadata__physical_number_isi,prov_metadata__workflow_state_tesim,id,desc_metadata__title_tesim,desc_metadata__genre_tesim,prov_metadata__barcode_tesi'
-      solr_response = solr.get 'select', :params => {:q => "in_box_ssim:info:fedora/#{box_id} AND active_fedora_model_ssi:PulStore::Lae::Folder", 
-                                                     :fl => fields_to_return, 
-                                                     :start => 0, 
-                                                     :rows => docs, 
-                                                     :wt => :ruby, 
-                                                     :index => true,
-                                                     :sort => "prov_metadata__physical_number_isi desc" }
-      solr_response['response']['docs']
+      q = "+in_box_ssim:\"info:fedora/#{box_id}\" +active_fedora_model_ssi:\"PulStore::Lae::Folder\""
+      docs = ActiveFedora::SolrService.query(q, sort: 'prov_metadata__physical_number_isi desc', rows: 99999)
+      docs
+      # solr = RSolr.connect(Blacklight.solr_config)
+      # fields_to_return = 'prov_metadata__physical_number_isi,prov_metadata__workflow_state_tesim,id,desc_metadata__title_tesim,desc_metadata__genre_tesim,prov_metadata__barcode_tesi'
+      # solr_response = solr.get 'select', :params => {:q => "in_box_ssim:info:fedora/#{box_id} AND active_fedora_model_ssi:PulStore::Lae::Folder", 
+      #                                                :fl => fields_to_return, 
+      #                                                :start => 0, 
+      #                                                :rows => docs, 
+      #                                                :wt => :ruby, 
+      #                                                :index => true,
+      #                                                :sort => "prov_metadata__physical_number_isi desc" }
+      # solr_response['response']['docs']
     end
 
   end

app/controllers/concerns/pul_store/lae/page_lookups.rb
@@ -3,16 +3,19 @@ module PulStore::Lae::PageLookups
   included do
 
     def get_pages_by_folder(folder_id)
-      solr = RSolr.connect(Blacklight.solr_config)
+      #solr = RSolr.connect(Blacklight.solr_config)
       # FIXME Get a default Where is the page sequence stored in index for this object?
       # sort by physical_number/sort_order
-      solr_response = solr.get 'select', :params => {:q => "is_part_of_ssim:info:fedora/#{folder_id} AND active_fedora_model_ssi:PulStore::Page", 
-                                                      :start => 0, 
-                                                      :rows => 400, 
-                                                      :wt => :ruby, 
-                                                      :index => true,
-                                                      :sort => 'desc_metadata__sort_order_isi asc' }
-      solr_response['response']['docs']
+      q = "+is_part_of_ssim:\"info:fedora/#{folder_id}\" +active_fedora_model_ssi:\"PulStore::Page\""
+      docs = ActiveFedora::SolrService.query(q, sort: 'desc_metadata__sort_order_isi asc', rows: 99999)
+      docs
+      # solr_response = solr.get 'select', :params => {:q => "is_part_of_ssim:info:fedora/#{folder_id} AND active_fedora_model_ssi:PulStore::Page", 
+      #                                                 :start => 0, 
+      #                                                 :rows => 400, 
+      #                                                 :wt => :ruby, 
+      #                                                 :index => true,
+      #                                                 :sort => 'desc_metadata__sort_order_isi asc' }
+      # solr_response['response']['docs']
     end
 
   end

app/views/pul_store/lae/boxes/_form.html.erb
@@ -93,9 +93,11 @@
         <span id="folder-count"><%= @box.folders.size %>&nbsp;</span>
       <% end %>
       Folders</h3>
+      <!--
       <div class="folder-options">
         <%= link_to "Show All Folders", "#{lae_box_path}/folders", remote: true, method: :get, :class => "btn btn-default show-all-folders" %>
       </div>
+      -->
     </div>
   <!-- start modal -->
         <div id="new-folder-modal" class="modal fade well" backdrop="static" role="dialog" aria-hidden="true">

spec/controllers/concerns/pul_store/lae/page_lookup_spec.rb
@@ -18,9 +18,10 @@ describe PulStore::Lae::PageLookups, :type => :controller do
     end
 
     it "Page Count by Folder should be Equal from Fedora and Solr" do
-      skip "Add test to retrive pages by folder from solr"
-      #folders = @box.folders
-      #expect(folders[0].pages.length).to eq(get_pages_by_folder(folders[0].id).length)
+      #skip "Add test to retrive pages by folder from solr"
+      folders = @box.folders
+      f = PulStore::Lae::Folder.find(folders[0].id)
+      expect(f.pages.size).to eq(get_pages_by_folder(folders[0].id).size)
     end
 
   end

spec/features/pul_store/lae/javascript/add_new_folders_spec.rb
@@ -41,7 +41,7 @@ feature "add new folders" do
     end
 
     scenario "New Folder Form Confirms Invalid Save", js: true do
-      skip("Passing locally but travis repeatedly throws errors") if ENV["CI"]
+      #skip("Passing locally but travis repeatedly throws errors") if ENV["CI"]
       boxes = Array.new(3) do |b|
         FactoryGirl.create(:lae_box, barcode: TEST_BARCODES.pop )
       end

spec/features/pul_store/lae/javascript/attach_hard_drive_spec.rb
@@ -18,7 +18,7 @@ feature "attach a hard drive" do
   end
 
     scenario "Attach New Hard Drive Confirms Success", js: true do
-      skip("Passing locally but travis repeatedly throws errors") if ENV["CI"]
+      #skip("Passing locally but travis repeatedly throws errors") if ENV["CI"]
       boxes = Array.new(3) do |b|
         FactoryGirl.create(:lae_box, barcode: TEST_BARCODES.pop )
       end
@@ -37,7 +37,7 @@ feature "attach a hard drive" do
     end
 
     scenario "Invalid Drive Barcode Confirms Failure", js: true do
-      skip("Passing locally but travis repeatedly throws errors") if ENV["CI"]
+      #skip("Passing locally but travis repeatedly throws errors") if ENV["CI"]
       # TESTME
       skip "Not sure duplicate barcode validation works here"
     end

spec/features/pul_store/lae/javascript/box_edit_page_spec.rb
@@ -16,7 +16,7 @@ feature 'Box Page Should Display Attached Folders' do
     User.delete_all
   end
 
-  scenario "When I load the Folder Page I See should see the default number of folders", js: true do
+  scenario "When I load the Folder Page I See should see an edit row for each attached folder", js: true do
     boxes = Array.new(3) do |b|
       FactoryGirl.create(:lae_box, barcode: TEST_BARCODES.pop )
     end
@@ -27,24 +27,26 @@ feature 'Box Page Should Display Attached Folders' do
     end
     login_as(@user, :scope => :user)
     visit edit_lae_box_path(box.id)
-    expect(page).to have_xpath('//table/tbody/tr', :count => 20)
-
-  end
-  scenario "When I Click the See all Folders Button I should see all attached Folders", js: true do
-    skip("Passing locally but travis repeatedly throws errors") if ENV["CI"]
-    boxes = Array.new(3) do |b|
-      FactoryGirl.create(:lae_box, barcode: TEST_BARCODES.pop )
-    end
-    box = boxes[rand(0..2)]
-    folders = Array.new(21) do |f|
-      #:lae_core_folder
-      FactoryGirl.create(:lae_core_folder, box: box, barcode: TEST_BARCODES.pop )
-    end
-    login_as(@user, :scope => :user)
-    visit edit_lae_box_path(box.id)
-    click_link('Show All Folders')
     expect(page).to have_xpath('//table/tbody/tr', :count => 21)
 
   end
+
+  # Now showing all folders 
+  # scenario "When I Click the See all Folders Button I should see all attached Folders", js: true do
+  #   #skip("Passing locally but travis repeatedly throws errors") if ENV["CI"]
+  #   boxes = Array.new(3) do |b|
+  #     FactoryGirl.create(:lae_box, barcode: TEST_BARCODES.pop )
+  #   end
+  #   box = boxes[rand(0..2)]
+  #   folders = Array.new(21) do |f|
+  #     #:lae_core_folder
+  #     FactoryGirl.create(:lae_core_folder, box: box, barcode: TEST_BARCODES.pop )
+  #   end
+  #   login_as(@user, :scope => :user)
+  #   visit edit_lae_box_path(box.id)
+  #   click_link('Show All Folders')
+  #   expect(page).to have_xpath('//table/tbody/tr', :count => 21)
+
+  # end
 
 end